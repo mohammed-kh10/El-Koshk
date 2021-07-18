b2455fe54b9ccf6ff7efc7d1ff49f9b1f6498a32
Mohammed Khaled <81472511+mohammed-kh10@users.noreply.github.com>
2021-07-18 17:54:06 +0200

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,8 @@
+<?php 
+include 'init.php';
+include $tpl . 'nav.php';
+include $tpl . 'header.php'?>
+
+
+
+<?php include $tpl . 'footer.php';?>
---
+++ admin/init.php
@@ -0,0 +1,14 @@
+<?php include 'config.php';
+// path
+// path
+$tpl = 'include/templates/';
+$css = 'layout/css/';
+$js = 'layout/js/';
+
+
+// include arabic language file
+include 'include/languages/ar.php';
+
+// include english language file
+include 'include/languages/en.php';
+?>
