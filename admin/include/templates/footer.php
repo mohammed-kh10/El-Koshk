54bad5c9f47615a340f94f7d9415869a5ea93615
Mohammed Khaled <81472511+mohammed-kh10@users.noreply.github.com>
2021-07-18 17:59:24 +0200

    Add files via upload

--- admin/include/templates/footer.php
+++ admin/include/templates/footer.php
@@ -1,8 +1,4 @@
-<h1>welcome from footer</h1>
-<h1>welcome from footer</h1>
-<h1>welcome from footer</h1>
-<h1>welcome from footer</h1>
-<h1>welcome from footer</h1>
+
 
 <script src="<?=$js?>external/jquery-3.5.1.min.js"></script>
 <script src="<?=$js?>external/aos.js"></script>
